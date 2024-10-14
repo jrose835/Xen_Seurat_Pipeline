8cc8f90a21b3628732b30aca0bb251b2d7582521
jrose835 <jrose835@gmail.com>
2024-10-14 15:51:46 -0400

    Added output documentation. Fixed lazy input file typo. Added plot_umap function to MultiResCluster. Built out image plots to account for all possible fov

--- src/Xen_Seruat_ClusterAnnot.R
+++ src/Xen_Seruat_ClusterAnnot.R
@@ -22,6 +22,11 @@
 
 
 # Outputs:
+#     - output/pipeline/objs Seurat object with UMAP and clustering at resolution specified as well as multiple other cluster resolutions
+#     - output/pipeline/umap UMAP plots at each resolution cluster
+#     - output/pipeline/clustree.png clustree graph plotted for each resolution range specified
+#     - output/visualization UMAP plots with both cluster ID and run ID to color each cell
+#     - output/visualization Image plots colored by 
 
 
 # Last updated: 10Oct2024
@@ -47,9 +52,9 @@ options(future.globals.maxSize = 16000 * 1024^2)
 #################################################
 #Inputs & Parameters
 
-indir <- "output/pipeline/objs" #--INPUT--
+indir <- "output/pipeline/objs" #--INPUT-- Note: If you used LoadQCPCA.R to produce you may not need to change
 infile <- "Abbie_lung_int-None_scTrans_PCA.rds" #--INPUT--
-input_obj_path <- here(indir, "Abbie_lung_int-None_scTrans_PCA.rds") 
+input_obj_path <- here(indir, infile) 
 
 experiment_name <- "Abbie_lung" #--INPUT--
 
@@ -74,7 +79,8 @@ obj <- RunUMAP(obj, dims=1:ndim, reduction=reduction)
 obj <- MultiResCluster(obj, res_range = c(0,1), 
                        outdir=here(outdir, "pipeline"), 
                        perform_clustering = TRUE,
-                       clustree=FALSE
+                       clustree=TRUE,
+                       plot_UMAP=TRUE
                        )
 Idents(obj) <- obj@meta.data[[paste0("res.",clus_res)]]
 
@@ -84,21 +90,34 @@ Idents(obj) <- obj@meta.data[[paste0("res.",clus_res)]]
 vis_out <- here(outdir, "visualization")
 dir.create.check(vis_out)
 
+###
+# UMAP plots
+
 umap1 <- DimPlot(obj, reduction="umap", raster.dpi=c(1024,1024))
 umap2 <- DimPlot(obj, reduction="umap", group.by=c("run_name"), raster.dpi=c(1024,1024))
+umap3 <- DimPlot(obj, reduction="umap", split.by=c("run_name"), raster.dpi=c(1024,1024))
 
 umaps <- umap1 + umap2
 ggsave(filename=here(vis_out, paste0(experiment_name, "_", "umaps.png")), plot=umaps, height=8, width=16)
 ggsave(filename=here(vis_out, paste0(experiment_name, "_", "umaps.pdf")), plot=umaps, height=8, width=16)
 
-fov1 <- ImageDimPlot(obj, fov="fov", group.by="seurat_clusters", size=0.75, border.color = "black")
-fov2 <- ImageDimPlot(obj, fov="fov.2", group.by="seurat_clusters",size=0.75, border.color = "black")
-#Need to change this to not be hardcoded and use as many fov as exist in object
+ggsave(filename=here(vis_out, paste0(experiment_name, "_", "runs_umaps.png")), plot=umap3, height=8, width=16)
+ggsave(filename=here(vis_out, paste0(experiment_name, "_", "runs_umaps.pdf")), plot=umap3, height=8, width=16)
 
-imgplot <- fov1 + fov2
-ggsave(filename=here(vis_out, paste0(experiment_name, "_", "imgclusters.png")), plot=imgplot, height=8, width=16)
-ggsave(filename=here(vis_out, paste0(experiment_name, "_", "imgclusters.pdf")), plot=imgplot, height=8, width=16)
+### 
+# Image plots
 
+img_plots_cmb <- ImageDimPlot(obj, fov=names(obj@images),size=0.75, border.color="black", combine=TRUE)
+img_plots_list <- ImageDimPlot(obj, fov=names(obj@images),size=0.75, border.color="black", combine=FALSE)
+names(img_plots_list) <- names(obj@images)
+
+ggsave(filename=here(vis_out, paste0(experiment_name, "_", "imgclusters.png")), plot=img_plots_cmb, height=8, width=16)
+ggsave(filename=here(vis_out, paste0(experiment_name, "_", "imgclusters.pdf")), plot=img_plots_cmb, height=8, width=16)
+
+for (i in 1:length(img_plots_list)){
+  ggsave(filename=here(vis_out, paste0(experiment_name, "_", names(img_plots_list)[i],"_imgclusters.png")), plot=img_plots_list[[i]], height=8, width=9)
+  ggsave(filename=here(vis_out, paste0(experiment_name, "_", names(img_plots_list)[i],"_imgclusters.pdf")), plot=img_plots_list[[i]], height=8, width=9)
+}
 
 #################################################
 #Automated annotation
